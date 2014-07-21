de47d6215489d079fcbcb0ac26a4d6c4066ee684
George Moura <gwmoura@gmail.com>
2014-07-20 21:39:36 -0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Churchmanager::Application.config.secret_key_base = '57968c07ae862e19bd3dd67dd4c365d20d5096a59b5217844df50dc752e58b12cbc6fe5be06aa5e9245034abfa64b5871855d965039f7e8a68748827796465e0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Churchmanager::Application.config.session_store :cookie_store, key: '_churchmanager_session'
