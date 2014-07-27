0404b24d8b7185708977cc6fad206a1b54bb2aa9
George Moura <gwmoura@gmail.com>
2014-07-27 19:23:31 -0300

    Change zip-code field for zip_code and change int to integer

--- db/migrate/20140727213404_create_members.rb
+++ db/migrate/20140727213404_create_members.rb
@@ -3,26 +3,26 @@ class CreateMembers < ActiveRecord::Migration
     create_table :members do |t|
       t.string :name
       t.date :birth_date
-      t.int :sex
+      t.integer :sex
       t.string :address
       t.string :neighborhood
       t.string :city
       t.string :state
       t.string :country
-      t.string :zip-code
+      t.string :zip_code
       t.string :home_phone
       t.string :mobile_phone
       t.string :work_phone
       t.string :email
       t.string :blood_type
-      t.int :marital_status
-      t.int :congregating
+      t.integer :marital_status
+      t.integer :congregating
       t.string :photo
       t.date :withdrawl_date
       t.references :departament, index: true
       t.string :skills
       t.date :worship_date
-      t.int :situation
+      t.integer :situation
       t.string :profession
       t.string :education
       t.date :baptism_date
