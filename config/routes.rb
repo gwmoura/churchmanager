9d72774a059dbd934aa91c4ab16a790705c1ea93
George Moura <gwmoura@gmail.com>
2014-07-29 21:50:53 -0300

    Add devise for users and change root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Churchmanager::Application.routes.draw do
+  devise_for :users
+  get "home/index"
   resources :prayer_requests
 
   resources :category_events
@@ -25,7 +27,7 @@ Churchmanager::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
