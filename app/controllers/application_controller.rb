964f6a2f68cea1975ec0cde2252c29370426ff07
George Moura <gwmoura@gmail.com>
2014-07-30 00:53:35 -0300

    Add translation by http

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_locale
+  #before_filter :set_locale_from_url
+
+  private
+
+  def set_locale
+    if lang = request.env['HTTP_ACCEPT_LANGUAGE']
+      lang = lang[/^[a-z]{2}/]
+      lang = :"pt-BR" if lang == "pt"
+    end
+    I18n.locale = params[:locale] || lang || I18n.default_locale
+  end
 end
