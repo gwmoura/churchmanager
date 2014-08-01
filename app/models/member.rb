5ad321fed5dde2da0d3f5692789644a6c30664cf
George Moura <gwmoura@gmail.com>
2014-07-31 21:34:28 -0300

    Add has_many e accepts_nested_attribute_for for member

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,7 @@ class Member < ActiveRecord::Base
   belongs_to :departament
   belongs_to :ministerial_role
   belongs_to :church
+
+  has_many :member_contacts
+  accepts_nested_attributes_for :member_contacts, :allow_destroy => true
 end
