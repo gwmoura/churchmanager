cada54f26946ac11fac6dd1749cea49916cb2844
George Moura <gwmoura@gmail.com>
2014-08-12 21:18:51 -0300

    Add coverall to rails

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,8 @@
+require 'coveralls'
+Coveralls.wear!('rails')
 ENV["RAILS_ENV"] ||= "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
-require 'coveralls'
-Coveralls.wear!
 
 class ActiveSupport::TestCase
   ActiveRecord::Migration.check_pending!
