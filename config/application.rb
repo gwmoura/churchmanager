f015b027dbba0460790adc743e8f471a660ca14e
George Moura <gwmoura@gmail.com>
2014-10-14 22:35:39 -0300

    Set config.i18n.enforce_available_locales to false and set default_locale = pt_BR

--- config/application.rb
+++ config/application.rb
@@ -15,11 +15,11 @@ module Churchmanager
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    #config.i18n.enforce_available_locales = false
+    config.i18n.enforce_available_locales = false
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :"pt-BR"
     config.encoding = "utf-8"
   end
 end
