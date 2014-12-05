8f16d4914291efe4d18df6c5217f9d5de129aa1e
George Moura <gwmoura@gmail.com>
2014-12-05 07:52:49 -0300

    Add one test to users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  setup do
+    sign_in :user, @user = users(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:users)
+  end
+
 end
